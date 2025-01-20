184118863f07f37f2a2d2f3af231b38a0551edca
mplatab <marcosplata98@gmail.com>
2025-01-20 16:03:29 -0500

    fix: loaded rest-api movie

---
+++ app.js
@@ -0,0 +1,161 @@
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
+const cors = require('cors')
+const express = require('express') // require -> commonJS
+const { validateMovie, validatePartialMovie } = require('./schema/movies')
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:3000',
+      'http://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+
+}))
+
+app.disable('x-powered-by') // deshabilitar del header x-powered-by: Express
+
+//  métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS Pre-Flight
+// OPTIONS
+
+// const ACCEPTED_ORIGINS = [
+//   'http://localhost:8080',
+//   'http://localhost:3000',
+//   'http://movies.com'
+// ]
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hola mundo' })
+})
+
+/* UNA FORMA DE SOLUCIONAR LOS CORS
+
+app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+
+  const { genre } = req.query
+  if (genre) {
+    const filterMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase())
+    )
+
+    return res.json(filterMovies)
+  }
+  res.json(movies)
+})
+*/
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filterMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase())
+    )
+
+    return res.json(filterMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // bases de datos
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+/* OTRA FORMA DE SOLUCIONAR LOS CORS CON DELETE TOCABA USAR options
+app.delete('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    res.status(404).json({ message: 'Movie not found' })
+  }
+  movies.splice(movieIndex, 1)
+  res.json({ message: 'Movie deleted' })
+})
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  }
+  res.sendStatus(200)
+})
+*/
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    res.status(404).json({ message: 'Movie not found' })
+  }
+  movies.splice(movieIndex, 1)
+  res.json({ message: 'Movie deleted' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT}`)
+})
